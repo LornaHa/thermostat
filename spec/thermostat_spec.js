8c1c06aad125b0a290d6bf22b77a49edc5edeece
leoncross <leon@lscross.co.uk>
2018-11-28 15:43:45 +0000

    wrote test for minimum temp and passed tests

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -1,4 +1,5 @@
 
+
 describe('Thermostat', function() {
   var thermostat;
 
@@ -9,35 +10,30 @@ describe('Thermostat', function() {
   describe('default status', function() {
     it('initializes the Thermostat with 20 degrees', function(){
       expect(thermostat.temperature).toEqual(20);
-
     });
   });
 
   describe('to increase temperature', function() {
     it('increases temperature by 1 degree', function() {
-      thermostat.up()
+      thermostat.up(1)
       expect(thermostat.temperature).toEqual(21);
     });
   });
 
   describe('to decrease temperature', function() {
-    it('increases temperature by 1 degree', function() {
-      thermostat.down()
+    it('decreases temperature by 1 degree', function() {
+      thermostat.down(1)
       expect(thermostat.temperature).toEqual(19);
     });
+    it('has doesnt go below the minimum temperature', function() {
+      thermostat.down(10)
+      expect(function() {
+        thermostat.down(10);
+      }).toThrowError('Exceeds minimum temperature');
+
+    });
+
   });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,11 +2,15 @@ function Thermostat() {
   this.temperature = 20
 }
 
-Thermostat.prototype.up = function() {
-  return this.temperature+=1
+Thermostat.prototype.up = function(number) {
+  return this.temperature+=number
 
 };
 
-Thermostat.prototype.down = function() {
-  return this.temperature-=1
+Thermostat.prototype.down = function(number) {
+  if (this.temperature <= 10) {
+    throw new Error('Exceeds minimum temperature');
+  } else {
+    return this.temperature-=number
+  }
 };
